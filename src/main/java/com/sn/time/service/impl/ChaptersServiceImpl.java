847722c43adad931158cafdc993639d386fd5065
songning <1457065857@qq.com>
2020-04-23 10:09:54 +0800

    更新连载中书籍失败

--- src/main/java/com/sn/time/service/impl/ChaptersServiceImpl.java
+++ src/main/java/com/sn/time/service/impl/ChaptersServiceImpl.java
@@ -38,44 +38,49 @@ public class ChaptersServiceImpl implements ChaptersService {
             src = elasticSearchDao.mustTermRangeQuery(novelsEsSearch, termParams, null);
             if (src != null && !src.isEmpty()) {
                 for (SearchResult.Hit<Object, Void> item : src) {
-                    String sourceUrl = String.valueOf(((Map) item.source).get("sourceUrl"));
-                    String novelsId = item.id;
-                    Document listDoc = HttpUtil.getHtmlFromUrl(sourceUrl, true);
-                    String latestChapter = listDoc.getElementById("info").getElementsByTag("p").get(3).getElementsByTag("a").get(0).html();
-                    List<Map<String, Object>> isExistList = this.findByNovelsId(novelsId);
-                    String lastChapter = String.valueOf(isExistList.get(isExistList.size() - 1).get("chapter"));
-                    if (!latestChapter.equals(lastChapter)) {
-                        Elements ddElements = listDoc.getElementById("list").getElementsByTag("dd");
-                        String oldUpdateTime = isExistList.get(isExistList.size() - 1).get("updateTime").toString();
-                        String strUpdateTime = listDoc.getElementById("info").getElementsByTag("p").get(2).html().split("：")[1];
-                        Date newUpdateTime = DateUtil.strToDate(strUpdateTime, "yyyy-MM-dd HH:mm:ss");
-                        List<String> timeList = DateUtil.stepTime(DateUtil.strToDate(oldUpdateTime, "yyyy-MM-dd HH:mm:ss"), newUpdateTime, ddElements.size() - isExistList.size());
-                        for (int i = isExistList.size(), iLen = ddElements.size(); i < iLen; i++) {
-                            Element chapterElement = ddElements.get(i).getElementsByTag("a").get(0);
-                            String chapter = chapterElement.html();
-                            boolean isExist = false;
-                            for (Map<String, Object> val : isExistList) {
-                                if (chapter.equals(String.valueOf(val.get("chapter")))) {
-                                    isExist = true;
-                                    break;
+                    try {
+                        String sourceUrl = String.valueOf(((Map) item.source).get("sourceUrl"));
+                        String novelsId = item.id;
+                        Document listDoc = HttpUtil.getHtmlFromUrl(sourceUrl, true);
+                        String latestChapter = listDoc.getElementById("info").getElementsByTag("p").get(3).getElementsByTag("a").get(0).html();
+                        List<Map<String, Object>> isExistList = this.findByNovelsId(novelsId);
+                        String lastChapter = String.valueOf(isExistList.get(isExistList.size() - 1).get("chapter"));
+                        if (!latestChapter.equals(lastChapter)) {
+                            Elements ddElements = listDoc.getElementById("list").getElementsByTag("dd");
+                            String oldUpdateTime = isExistList.get(isExistList.size() - 1).get("updateTime").toString();
+                            String strUpdateTime = listDoc.getElementById("info").getElementsByTag("p").get(2).html().split("：")[1];
+                            Date newUpdateTime = DateUtil.strToDate(strUpdateTime, "yyyy-MM-dd HH:mm:ss");
+                            List<String> timeList = DateUtil.stepTime(DateUtil.strToDate(oldUpdateTime, "yyyy-MM-dd HH:mm:ss"), newUpdateTime, ddElements.size() - isExistList.size());
+                            for (int i = isExistList.size(), iLen = ddElements.size(); i < iLen; i++) {
+                                Element chapterElement = ddElements.get(i).getElementsByTag("a").get(0);
+                                String chapter = chapterElement.html();
+                                boolean isExist = false;
+                                for (Map<String, Object> val : isExistList) {
+                                    if (chapter.equals(String.valueOf(val.get("chapter")))) {
+                                        isExist = true;
+                                        break;
+                                    }
                                 }
-                            }
-                            if (!isExist) {
-                                String contentUrl = "http://www.147xiaoshuo.com/" + chapterElement.attr("href");
-                                String content = "暂无资源...";
-                                try {
-                                    Document contentDoc = HttpUtil.getHtmlFromUrl(contentUrl, true);
-                                    content = contentDoc.getElementById("content").html();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                    log.error("更新(latestChapter)最新章节contentUrl: {} fail", contentUrl);
+                                if (!isExist) {
+                                    String contentUrl = "http://www.147xiaoshuo.com/" + chapterElement.attr("href");
+                                    String content = "暂无资源...";
+                                    try {
+                                        Document contentDoc = HttpUtil.getHtmlFromUrl(contentUrl, true);
+                                        content = contentDoc.getElementById("content").html();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        log.error("更新(latestChapter)最新章节contentUrl: {} fail", contentUrl);
+                                    }
+                                    Chapters chapters = Chapters.builder().chapter(chapter).content(content).contentUrl(contentUrl).novelsId(novelsId).updateTime(timeList.get(i - isExistList.size())).build();
+                                    ElasticSearch chaptersEsSearch = ElasticSearch.builder().index("chapters_index").type("chapters").build();
+                                    elasticSearchDao.save(chaptersEsSearch, chapters);
+                                    log.info("当前小说novelsId: {}; 更新章节chapter: {}", novelsId, chapter);
                                 }
-                                Chapters chapters = Chapters.builder().chapter(chapter).content(content).contentUrl(contentUrl).novelsId(novelsId).updateTime(timeList.get(i - isExistList.size())).build();
-                                ElasticSearch chaptersEsSearch = ElasticSearch.builder().index("chapters_index").type("chapters").build();
-                                elasticSearchDao.save(chaptersEsSearch, chapters);
-                                log.info("当前小说novelsId: {}; 更新章节chapter: {}", novelsId, chapter);
                             }
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        log.error("更新连载中书籍失败 {}", item);
                     }
                 }
             } else {
